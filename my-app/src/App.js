1446b9d1b9ec940d948105f919c121ad70ba0ed8
Berkay <berkay.ergun16@gmail.com>
2021-07-06 17:15:47 +0300

    13end

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,29 @@ import MovieList from './MovieList';
 class App extends React.Component{
   
   state=  {
-
+    movies:[
+      {
+        "id":1,
+        "name":"The Flash",
+        "rating":8.3,
+        "overview":"Just add your desired image size (width & height) after our URL, and you'll get a random image"
+  
+      },
+      {
+        "id":2,
+        "name":"İnterstaller",
+        "rating":6.8,
+        "overview":"Just add your desired image size (width & height) after our URL, and you'll get a random image"
+  
+      },
+      {
+        "id":3,
+        "name":"Arrow",
+        "rating":7.9,
+        "overview":"Just add your desired image size (width & height) after our URL, and you'll get a random image"
+  
+      }
+    ]
   }
 
   render(){
@@ -14,10 +36,10 @@ class App extends React.Component{
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
-            <SearchBar/>
+           
           </div>
         </div>
-        <MovieList/>
+        <MovieList movies={this.state.movies}/>
       </div>
     );
   }
--- my-app/src/MovieList.js
+++ my-app/src/MovieList.js
@@ -1,23 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class MovieList extends Component {
-    render() {
+export default function MovieList(props) {
+    
+   
         return (
             <div className="row">
-                <div className="col-lg-4">
-                    <div className="card mb-4 shadow-sm">
-                        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/y1TKElkgWOl90iFik3GHlgJdphm.jpg" className="card-img-top" alt="sample Movie"/>
-                        <div className="card-body">
-                            <h5 className="card-title">Sample Movie</h5>
-                            <p className="cart-text">Sample Movie Description</p>
-                            <div className="d-flex justify-content-between align-items-center">
-                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
-                                <h2><span className="badge badge-pill badge-primary bg-dark">9.0</span></h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {props.movies.map((movie)=>(
+                         <div className="col-lg-4" key={movie.id}>
+                         <div className="card mb-4 shadow-sm">
+                             <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/y1TKElkgWOl90iFik3GHlgJdphm.jpg" className="card-img-top" alt="sample Movie"/>
+                             <div className="card-body">
+                                 <h5 className="card-title">{movie.name}</h5>
+                                 <p className="cart-text">{movie.overview}</p>
+                                 <div className="d-flex justify-content-between align-items-center">
+                                     <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
+                                     <h2><span className="badge badge-pill badge-primary bg-dark">{movie.rating}</span></h2>
+                                 </div>
+                             </div>
+                         </div>
+                     </div>
+                ))}
             </div>
         )
-    }
+    
 }

7e3bb884f98b46781cc54a5f42d1b6c3df33a1ce
Berkay <berkay.ergun16@gmail.com>
2021-07-06 16:20:04 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component{
+  
+  state=  {
 
-  state = {
-    durum: false
   }
 
-
   render(){
+
     return (
       <div >
         <h1 >My Movies</h1>

e0f86112c4c7f39a0d1f749e495803621b005871
Berkay <berkay.ergun16@gmail.com>
2021-07-08 18:44:47 +0300

    18end
    jsondan gelen veriyi, setstate ile boş moviese atadık. ComponentdidMount kullandık.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,70 +3,27 @@ import "./App.css";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
 class App extends React.Component {
+  //fetch, asenkron olarak network sorguları yapmamızı sağlayan fonksiyon
+  //bize bir promise döner.
   state = {
-    movies: [
-      {
-        id: 1,
-        name: "The Flash",
-        rating: 8.3,
-        overview:
-          "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg",
-      },
-
-      {
-        id: 2,
-        name: "Interstellar",
-        rating: 6.8,
-        overview:
-          "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
-      },
-
-      {
-        id: 3,
-        name: "Arrow",
-        rating: 7.9,
-        overview:
-          "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg",
-      },
-
-      {
-        id: 4,
-        name: "Rogue",
-        rating: 7.4,
-        overview:
-          "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
-      },
-
-      {
-        id: 5,
-        name: "Project Power",
-        rating: 6.7,
-        overview:
-          "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg",
-      },
-
-      {
-        id: 6,
-        name: "Superman",
-        rating: 7.6,
-        overview:
-          "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        imageURL:
-          "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg",
-      },
-    ],
+    movies:[],
     searchQuery: "",
   };
+//UI compları dom da yerini aldıktan sonra hemen çalışıyodu.
+//Dışarıdan http isteği yapıcaksak bu metodu kullanmalıyız.
+  
+ async  componentDidMount(){
+    const baseURL = "http://localhost:3000/movies"
+    const response = await fetch(baseURL)
+    //promise dönüyor. Bize json tabanlı bir dosya lazım
+    //fetch asenkron döndüğü için jsona çevirdiğimiz de hata veriyor.
+    // o yüzden fonksiyonumuzu asenkorn haline getirmemiz gerek.
+    //await ve async
+    console.log(response)
+    const data = await response.json();
+    console.log(data)
+    this.setState({movies : data})
+  }
 
   deleteMovie = (movie) => {
     const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);

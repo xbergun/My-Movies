ceb0c57c859c6583ce85c440158165cec613f1f4
Berkay <berkay.ergun16@gmail.com>
2021-07-22 15:47:28 +0300

    ders21 restapi added.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,12 @@ import "./App.css";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
 import axios from 'axios'
+require('dotenv').config()
+
+console.log(process.env.REACT_APP_API_KEY)
+
+
+
 class App extends React.Component {
   //fetch, asenkron olarak network sorguları yapmamızı sağlayan fonksiyon
   //bize bir promise döner.
@@ -10,57 +16,18 @@ class App extends React.Component {
     movies:[],
     searchQuery: "",
   };
-//UI compları dom da yerini aldıktan sonra hemen çalışıyodu.
-//Dışarıdan http isteği yapıcaksak bu metodu kullanmalıyız.
-  
-//  async  componentDidMount(){
-//     const baseURL = "http://localhost:3000/movies"
-//     const response = await fetch(baseURL)
-//     //promise dönüyor. Bize json tabanlı bir dosya lazım
-//     //fetch asenkron döndüğü için jsona çevirdiğimiz de hata veriyor.
-//     // o yüzden fonksiyonumuzu asenkorn haline getirmemiz gerek.
-//     //await ve async
-//     console.log(response)
-//     const data = await response.json();
-//     console.log(data)
-//     this.setState({movies : data})
-//   }
-
+                                                                           
 //axios ile get ve delete request
   async  componentDidMount(){
     //fetch ile, responseyi json formatına çeviriyoduk. Axios direkt json formatında nesne döndürüyor.
-   const response = await axios.get("http://localhost:3000/movies")
-   this.setState({movies: response.data})
-  // console.log(response)
+   //const response = await axios.get(`https://api.themoviedb.org/3/list/7102272?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`) GERÇEK APİDEN ALDIĞIMIZ
+
+   const response = await axios.get(`272?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+   console.log(response.data.items)
+  this.setState({movies: response.data.items})
   }
 
 
-  // deleteMovie = (movie) => {
-  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
-  //   this.setState({
-  //     movies: newMovieList,
-  //   });
-  //   //farklı bir patterni daha var.
-  //   this.setState((state) => ({
-  //     movies: newMovieList,
-  //   }));
-  // };
-  
-  //FETCH APİ İLE OLUŞTURULAN DELETE
-  // deleteMovie = async (movie) => {
-  //   const baseURL = `http://localhost:3000/movies/${movie.id}`
-  //   await fetch(baseURL, {
-  //     method:"DELETE"
-  //   })
-  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
-  //   this.setState({
-  //     movies: newMovieList,
-  //   });
-  //   //farklı bir patterni daha var.
-  //   this.setState((state) => ({
-  //     movies: newMovieList,
-  //   }));
-  // };
 
   //AXİOS APİ İLE OLUŞTURULAN DELETE
   deleteMovie = async (movie) => {
@@ -83,7 +50,7 @@ class App extends React.Component {
   render() {
     //
     let filteredMovies =this.state.movies.filter((movie) =>{
-      return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
+      return movie.title.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
       //aradağımız kelime, eğer indexof un içinde varsa o return eder.
     })
     return (
@@ -113,3 +80,52 @@ export default App;
 //senkron, aynı anda çalışabilen, asenkron da aynı anda çalışamayan
 //responsu alana kadar bekle diyoruz. Ondan sonra ypaıcaksın o işi.
 //awaitten sonra get diyoruz o isteği alana kadar bekle. Alınca aşağıdaki koda geç anlamında.
+
+
+
+
+
+
+     //UI compları dom da yerini aldıktan sonra hemen çalışıyodu.
+               //Dışarıdan http isteği yapıcaksak bu metodu kullanmalıyız.
+                                                                                  
+                                                                                //  async  componentDidMount(){
+                                        //     const baseURL = "http://localhost:3000/movies"
+                                                                                //     const response = await fetch(baseURL)
+                                                                                //     //promise dönüyor. Bize json tabanlı bir dosya lazım
+                                                                                //     //fetch asenkron döndüğü için jsona çevirdiğimiz de hata veriyor.
+                                                                                //     // o yüzden fonksiyonumuzu asenkorn haline getirmemiz gerek.
+                                                                                //     //await ve async
+                                                                                //     console.log(response)
+                                                                                //     const data = await response.json();
+                                                                                //     console.log(data)
+                                                                                //     this.setState({movies : data})
+                                                                                //   }
+
+                                                                                
+  // deleteMovie = (movie) => {
+  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+  //   this.setState({
+  //     movies: newMovieList,
+  //   });
+  //   //farklı bir patterni daha var.
+  //   this.setState((state) => ({
+  //     movies: newMovieList,
+  //   }));
+  // };
+  
+  //FETCH APİ İLE OLUŞTURULAN DELETE
+  // deleteMovie = async (movie) => {
+  //   const baseURL = `http://localhost:3000/movies/${movie.id}`
+  //   await fetch(baseURL, {
+  //     method:"DELETE"
+  //   })
+  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+  //   this.setState({
+  //     movies: newMovieList,
+  //   });
+  //   //farklı bir patterni daha var.
+  //   this.setState((state) => ({
+  //     movies: newMovieList,
+  //   }));
+  // };
